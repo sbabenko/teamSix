e51f38ad2c7ac9b756c6071b7a4ed43c39563df7
st8 <sxenko@gmail.com>
2018-09-19 16:51:53 -0400

    Added incomingEvent Model
    Added abstract model to firstResponderDashboard. Should figure out where to put model later, this may not be final location. May have to change project architecture into one application instead to have these models global

--- fireM2/firstResponderDashboard/models.py
+++ fireM2/firstResponderDashboard/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import reverse #Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
+class incomingEvent(models.Model):
+    """Model representing an incoming call for service"""
+    location = models.CharField(max_length=200, help_text='Coordinates for incoming service event')
+    serviceType = models.CharField(max_length=200, help_text='Description of service needed')
+    urgency = models.CharField(max_length=200, help_text='Urgency level of situation')
+    time = models.CharField(max_length=200, help_text='Time the event came in')
+
+    class Meta:
+        ordering = [''] #figure out how to sort by urgency, and then oldest (get most urgent events first, then sort by oldest?)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.urgency
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular event instance."""
+        return reverse('', args=[str(self.id)])
