06e6688b3fbcfe1ae7dbe69d177f40f51bca1dd6
st8 <sxenko@gmail.com>
2018-09-22 11:41:15 -0400

    Added Rudimentary Login/Logout

--- fireM2/dashBoard/views.py
+++ fireM2/dashBoard/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from events.models import *
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
     """View function for home page of site."""
 
--- fireM2/fireM2/settings.py
+++ fireM2/fireM2/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -125,3 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
