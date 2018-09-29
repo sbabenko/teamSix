35a77325bde51d8cb1fa188ba165a395e62d0ab7
st8 <sxenko@gmail.com>
2018-09-29 12:42:01 -0400

    Added redirects by groups

--- fireM2/dashBoard/views.py
+++ fireM2/dashBoard/views.py
@@ -5,6 +5,10 @@ from events.models import *
 
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
+
 
 @never_cache
 @login_required
@@ -15,5 +19,14 @@ def index(request):
       
     }
 
+    if request.user.groups.filter(name="First Responders").exists():
+        # user is an admin
+        return redirect("first_respond_dash_view")
+    elif request.user.groups.filter(name="Mission Management Specialists").exists():
+        return redirect("miss_mgmt_dash_view")
+    elif request.user.groups.filter(name="Operations Chiefs").exists():
+        return redirect("ops_chief_dash_view")
+    else:
+        return render(request, 'index.html', context=context)
     # Render the HTML template index.html with the data in the context variable
-    return render(request, 'index.html', context=context)
+  
