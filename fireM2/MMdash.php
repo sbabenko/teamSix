cd840025943dd0158e8c984517ca9d4852807311
Aditya Kaliappan <adityak1@umbc.edu>
2018-10-18 16:34:08 -0400

    Added check for user role before allowing access to individual dashboards.

--- fireM2/MMdash.php
+++ fireM2/MMdash.php
@@ -7,16 +7,20 @@ session_start();
 // Check if user is logged in using the session variable
 if ( $_SESSION['logged_in'] != 1 ) {
     $_SESSION['message'] = "You must log in before viewing your profile page!";
-    header("location: error.php");    
-  }
-  else {
-      // Makes it easier to read
-      $first_name = $_SESSION['first_name'];
-      $last_name = $_SESSION['last_name'];
-      $email = $_SESSION['email'];
-      $active = $_SESSION['active'];
-  
-  }
+    header("location: error.php");
+}
+//verify the user class before allowing access
+else if ($_SESSION['role'] != 'MM'){
+    $_SESSION['message'] = "You do not have the credentials to view this page!";
+    header("location: error.php");
+}
+else {
+    // Makes it easier to read
+    $first_name = $_SESSION['first_name'];
+    $last_name = $_SESSION['last_name'];
+    $email = $_SESSION['email'];
+    $active = $_SESSION['active'];
+}
 
 ?>
 
