39c7af03badcafa6b74441fbd457df1cfbaca9af
st8 <sxenko@gmail.com>
2018-10-25 18:03:06 -0400

    removed sign up

--- fireM2/index.php
+++ fireM2/index.php
@@ -33,8 +33,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   <div class="form">
       
       <ul class="tab-group">
-        <li class="tab"><a href="#signup">Sign Up</a></li>
-        <li class="tab active"><a href="#login">Login</a></li>
+       <!--<li class="tab"><a href="#signup">Sign Up</a></li>
+        <li class="tab active"><a href="#login">Login</a></li>-->
       </ul>
       
       <div class="tab-content">
