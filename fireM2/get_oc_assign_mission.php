9c0dadb20d214def344a4d5a962bdb9dc123da1a
Aditya Kaliappan <adityak1@umbc.edu>
2018-11-07 18:06:23 -0500

    Modified Added Basic Table for OC Assign to Mission

---
+++ fireM2/get_oc_assign_mission.php
@@ -0,0 +1,60 @@
+<?php
+require("db.php");
+
+function parseToXML($htmlStr)
+{
+$xmlStr=str_replace('<','&lt;',$htmlStr);
+$xmlStr=str_replace('>','&gt;',$xmlStr);
+$xmlStr=str_replace('"','&quot;',$xmlStr);
+$xmlStr=str_replace("'",'&#39;',$xmlStr);
+$xmlStr=str_replace("&",'&amp;',$xmlStr);
+return $xmlStr;
+}
+
+// Set the active MySQL database
+$db_selected = mysqli_select_db( $mysqli ,"FIREM2");
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysqli_error());
+}
+
+// Select all unassigned events
+$query = "SELECT * FROM mmEvent where missionID IS NULL";
+
+$result = $mysqli->query($query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error($mysqli));
+}
+
+header("Content-type: text/xml");
+
+// Start XML file, echo parent node
+echo "<?xml version='1.0' ?>";
+
+//create div to hold table
+echo "<div class = 'eventInfo'>";
+
+//create table of general information
+echo '<h1>Assign to Mission</h1>';
+echo '<table>';
+echo '<tr>';
+echo '<th>Event Name</th>';
+echo '<th>Mission Name</th>';
+echo '<th>Delete?</th>';
+echo '</tr>';
+
+$ind=0;
+// Iterate through the rows
+while ($row = @mysqli_fetch_assoc($result)){
+  echo '<tr>';
+  echo '<td>' . $row["eventName"] . '</td>';
+  echo '</tr>';
+
+  $ind = $ind + 1;
+}
+
+echo '</table>';
+
+// End XML file
+echo '</div>';
+
+?>
