474caebb6efbe66aa358a1fad35034bab2059302
Aditya Kaliappan <adityak1@umbc.edu>
2018-11-10 09:03:33 -0500

    Added Temporary Checkboxes for Category and Submission Method in Map Toggle

--- fireM2/incident_map_toggle.php
+++ fireM2/incident_map_toggle.php
@@ -7,17 +7,110 @@ if(!defined('OC_Tab') && !defined('MM_Tab')) {
 ?>
 
 <div class="sidePane">
+    <!-- display map type options -->
     <h2>Map Type</h2>
+
+    <table>
+        <tr>
+            <td>
+                <input type="radio" name="mapType" onclick="dispPoints(true)" checked>
+                <label>Pinpoints</label>
+            </td>
+            <td>
+                <input type="radio" name="mapType" onclick="dispPoints(false)">
+                <label>Heatmap</label>
+            </td>
+        </tr>
+    </table>
+
+    <br>
+
+    <!-- display event category options -->
+    <h2>Category</h2>
+
+    <table>
+        <tr>
+            <td>
+                <input type="checkbox" name="category" value="hurricane" onChange="toggleCategory(this)" checked>
+                <label>Hurricane</label>
+            </td>
+            <td>
+                <input type="checkbox" name="category" value="landslide" onChange="toggleCategory(this)" checked>
+                <label>Landslide</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="category" value="flood" onChange="toggleCategory(this)" checked>
+                <label>Flood</label>
+            </td>
+            <td>
+                <input type="checkbox" name="category" value="sinkhole" onChange="toggleCategory(this)" checked>
+                <label>Sinkhole</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="category" value="tsunami" onChange="toggleCategory(this)" checked>
+                <label>Tsunami</label>
+            </td>
+            <td>
+                <input type="checkbox" name="category" value="volcano" onChange="toggleCategory(this)" checked>
+                <label>Volcano</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="category" value="fire" onChange="toggleCategory(this)" checked>
+                <label>Fire</label>
+            </td>
+            <td>
+                <input type="checkbox" name="category" value="tornado" onChange="toggleCategory(this)" checked>
+                <label>Tornado</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="category" value="earthquake" onChange="toggleCategory(this)" checked>
+                <label>Earthquake</label>
+            </td>
+            <td>
+                <input type="checkbox" name="category" value="naturalGas" onChange="toggleCategory(this)" checked>
+                <label>Natural Gas</label>
+            </td>
+        </tr>
+    </table>
+
+    <br>
+
+    <!-- display event submission method options -->
+    <h2>Submission Method</h2>
     
     <table>
         <tr>
             <td>
-                Pinpoints
-                <input type="radio" name="mapType" onclick="dispPoints(true)" checked>
+                <input type="checkbox" name="submitMethod" value="phone" onChange="toggleSubmitMethod(this)" checked>
+                <label>Phone</label>
             </td>
             <td>
-                Heatmap
-                <input type="radio" name="mapType" onclick="dispPoints(false)">
+                <input type="checkbox" name="submitMethod" value="sms" onChange="toggleSubmitMethod(this)" checked>
+                <label>SMS</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="submitMethod" value="email" onChange="toggleSubmitMethod(this)" checked>
+                <label>Email</label>
+            </td>
+            <td>
+                <input type="checkbox" name="submitMethod" value="twitter" onChange="toggleSubmitMethod(this)" checked>
+                <label>Twitter</label>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="checkbox" name="submitMethod" value="facebook" onChange="toggleSubmitMethod(this)" checked>
+                <label>Facebook</label>
             </td>
         </tr>
     </table>
