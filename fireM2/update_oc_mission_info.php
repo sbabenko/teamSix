eecc1c5faec5cdb41ea82ff61481c330c41d9dae
Aditya Kaliappan <adityak1@umbc.edu>
2018-11-12 23:46:45 -0500

    Added Code to Update Mission to Completed State

---
+++ fireM2/update_oc_mission_info.php
@@ -0,0 +1,29 @@
+<?php
+require("db.php");
+
+// Set the active MySQL database
+$db_selected = mysqli_select_db( $mysqli ,"FIREM2");
+if (!$db_selected) {
+  die ('Can\'t use db : ' . mysqli_error());
+}
+
+//query to update mission to inactive
+$query = "UPDATE mission SET isActive = false WHERE missionID = " 
+    . $_POST["missionID"];
+
+$result = $mysqli->query($query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error($mysqli));
+}
+
+//query to add resources back to table
+$query = "UPDATE resource, resourceMission SET resource.quantity = " .
+    "resource.quantity + resourceMission.quantity WHERE resource.resourceID " .
+    "= resourceMission.resourceID AND missionID = " . $_POST["missionID"];
+
+$result = $mysqli->query($query);
+if (!$result) {
+  die('Invalid query: ' . mysqli_error($mysqli));
+}
+
+?>
