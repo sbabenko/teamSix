89dd907a49c67d54fea83f12c3ef1e97f28eb1ad
Aditya Kaliappan <adityak1@umbc.edu>
2018-11-16 12:40:40 -0500

    Added Python Script for Admin to Update Account Password

---
+++ fireM2/adminTools/updatePassword.py
@@ -0,0 +1,84 @@
+#References:
+#https://stackoverflow.com/questions/31291317/php-is-not-recognized-as-an-
+#     internal-or-external-command-in-command-prompt
+#https://stackoverflow.com/questions/48159538/passing-post-variable-with-
+#     subprocess-popen
+#https://stackoverflow.com/questions/12605498/how-to-use-subprocess-popen-python
+
+import subprocess
+import mysql.connector
+
+#display header for program
+print("Update User Account Password")
+
+#connect to database
+fireM2db = mysql.connector.connect(
+    host="mysql-instance1.crdymdfwdzej.us-east-1.rds.amazonaws.com",
+    user="root",
+    passwd="GucciSwag420",
+    database="FIREM2")
+
+#create cursor to perform queries
+mycursor = fireM2db.cursor()
+
+#create query to get all user emails in database
+sql = "select email from userAccount"
+
+#execute query
+mycursor.execute(sql)
+
+#get all email results
+results = mycursor.fetchall()
+
+#display all available emails
+counter = 0
+for element in results:
+    #display email in list format
+    print(str(counter + 1) + ". " + element[0])
+    counter = counter + 1
+
+#get email selection
+if(counter > 0):
+    #select one email
+    email = ""
+    while(True):
+        #get email entry number
+        emailID = int(raw_input("Select email number (ex. 1): "))
+
+        #validate that entry is within range
+        if(emailID > 0 and emailID <= counter):
+            email = results[emailID - 1][0]
+            break;
+
+    #get new password
+    password = ""
+    while(len(password) == 0):
+        #get password input
+        password = raw_input("Enter password: ")
+
+        #validate that nonempty password is entered
+        if(len(password) == 0):
+            password = ""
+
+    #run PHP script to hash password
+    proc = subprocess.Popen(['php', 'hashPassword.php', password],
+                            stdout = subprocess.PIPE)
+
+    #get password hash
+    passHash = proc.stdout.readline()
+
+    #create query to insert account into database
+    sql = "update userAccount set loginPass = %s where email = %s"
+    val = (passHash, email)
+
+    #execute query
+    mycursor.execute(sql, val)
+
+    #commit changes to database
+    fireM2db.commit()
+
+    #display success message
+    print("User password successfully updated.")
+else:
+    #display error message for no available accounts
+    print("No accounts available.")
