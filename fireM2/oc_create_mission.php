c7e2d9a23b43783bb2504c49d77b405f3fa074c2
st8 <sxenko@gmail.com>
2018-11-17 22:38:26 -0500

    Update oc_create_mission.php

--- fireM2/oc_create_mission.php
+++ fireM2/oc_create_mission.php
@@ -22,71 +22,14 @@ $(document).ready(function() {
     //},1000);//end setInterval
 });//end docReady 
 </script>
-
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "";
-  } else {
-    $name = test_input($_POST["name"]);
-    // check if name only contains letters and whitespace
-    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed"; 
-    }
-  }
-  
-  if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    // check if e-mail address is well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format"; 
-    }
-  }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL"; 
-    }
-  }
-
-  if (empty($_POST["comment"])) {
-    $comment = "";
-  } else {
-    $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
-  }
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
 <script>
-
 function validateForm() {
     alert("Mission name validation triggered");
     var x = document.forms["myform"]["input_mission_name"].value;
     if (x == "") {
         alert("Mission name must be filled out");
+        document.getElementById("myform").reset();
+        throw new Error("Something went badly wrong!");
         return false;
     }
 }
@@ -97,7 +40,7 @@ function validateForm() {
   <label align="center" style="opacity: 0.5;position:relative;left:60px;top:30px;
     font-size: 20px;">Mission Name</label>
   <input autofocus maxlength="40" style="display:block;width:80%;margin:auto;" type="text" name="input_mission_name">
-  <span class="error"><?php echo $nameErr;?></span>
+ 
   
 
   <!-- Resources objects generated from inside "get_resources.php" 
