55bbf7cba30d9ce10c23a919d2145cfdb20e4aec
sbabenko <st8@umbc.edu>
2018-11-29 11:19:18 -0500

    Added HTTPS to CDN paths

--- fireM2/MMdash.php
+++ fireM2/MMdash.php
@@ -91,7 +91,7 @@ define('MM_Tab', TRUE);
 
     <!-- included for a bunch of javascript libraries -->
 
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="js/index.js"></script>
 
     <script>
--- fireM2/index.php
+++ fireM2/index.php
@@ -64,7 +64,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <?php include 'incident_map.php'; ?>
     <?php include 'clock.php'; ?>
 
-    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
     <script src="js/index.js"></script>
 
     <script>
