f4759e5a526201c3d9b2df779c21e7b04d8d9bc3
sbabenko <st8@umbc.edu>
2019-06-28 10:46:28 -0400

    Update db.php

--- fireM2/db.php
+++ fireM2/db.php
@@ -20,8 +20,8 @@
 
 /* Database connection settings */
 $host = 'mysql-instance1.crdymdfwdzej.us-east-1.rds.amazonaws.com';
-$user = 'root';
-$pass = 'GucciSwag420';
+$user = '###########';
+$pass = '###########';
 $db = 'FIREM2';
 
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
